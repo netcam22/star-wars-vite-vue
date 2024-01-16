0b78d0e7cd3b07dd84ace552202a6e0bae730620
netcam22 <annettelesage@btinternet.com>
2024-01-16 14:46:36 +0000

    feat: fetch data from swapi

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,18 @@ import SubHeading from './components/SubHeading.vue';
     <Heading title="Star Wars" />
     <SubHeading text="A long time ago in a galaxy far, far away...." />
         <section className ="page__container">
-        <Character image = '/src/assets/images/luke.jpeg' name = "Luke" 
-        height =  "172" />
-        <Character image = '/src/assets/images/vader.jpeg' name = "Darth Vader" 
-        height =  "202" />
-        <Character image = '/src/assets/images/c3p0.jpeg' name = "SeeThreePio" 
-        height =  "167" />
-        <Character image = '/src/assets/images/R2D2.jpeg' name = "Artoo Deetoo" 
-        height =  "96" />
+          <Suspense>
+        <Character image = '/src/assets/images/luke.jpeg'/>
+      </Suspense>
+      <Suspense>
+        <Character image = '/src/assets/images/vader.jpeg' />
+      </Suspense>
+      <Suspense>
+        <Character image = '/src/assets/images/c3p0.jpeg' />
+      </Suspense>
+      <Suspense>
+        <Character image = '/src/assets/images/R2D2.jpeg' />
+      </Suspense>
         </section>
         </main>
 </template>
