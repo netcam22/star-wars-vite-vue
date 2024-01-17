c065466fd70dab9ef9631c25b6b7732dcf1fddc4
netcam22 <annettelesage@btinternet.com>
2024-01-17 07:29:35 +0000

    feat: implement useFetch custom hook for fetching swapi data

---
+++ src/components/CharacterContainer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const characters = [{id: '1', image: '/src/assets/images/luke.jpeg'}, 
+{id: '4', image: '/src/assets/images/vader.jpeg'}, 
+{id: '2', image: '/src/assets/images/c3p0.jpeg'}, 
+{id: '3', image: '/src/assets/images/R2D2.jpeg'}];
+
+import Character from "./Character.vue";
+
+</script>
+
+<template>
+
+<section className ="page__container">
+    <Character v-for="(character) in characters"
+    :key="character.id" 
+    :image="character.image" :characterId="character.id" />
+</section>
+</template>
